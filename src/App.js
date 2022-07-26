22d9a5b2c58aab64b30acbe4abba4ca87f0f81d2
cephasDevelop <cephaspc@outlook.com>
2022-07-26 14:49:22 +0300

    first folder structure

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter,Routes, Route } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
+import Projects from './components/Pages/Projects/Projects';
+import Experience from './components/Pages/Experience/Experience';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home/>} />
+          <Route path='/projects' element={ <Projects/>} />
+          <Route path='/experience' element={ <Experience/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
