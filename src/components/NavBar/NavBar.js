08462d61c8c234c0261981ca29d403f054769875
cephasDevelop <cephaspc@outlook.com>
2022-07-26 17:17:59 +0300

    navbarStyle

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 const NavBar = () => {
     return (
         <div className='el-navbar'>
             <div className='el-toggleButton'>
-                <button></button>
+                <button><ReorderIcon/></button>
             </div>
             <div className='el-links'>
                 <Link to='/'>Home</Link>
